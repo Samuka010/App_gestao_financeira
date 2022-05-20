5dc934f197cb524a2814d4b8c05a58b6ad5dadf0
Samuka010 <samuelcsp123@gmail.com>
2022-05-19 21:44:31 -0300

    Melhoria na interface e traducao

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :movimentacoes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :movimentacoes, only: [:index, :new, :create, :destroy]
+  root "movimentacoes#index"
 end
